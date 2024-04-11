e4be7f48d0ab12fef7ae5c915173b6a5bb0da195
Matthew Hutter <matthewjhutter@gmail.com>
2024-04-11 12:41:27 -0700

    Added fact about GitHub office locations

--- facts.sh
+++ facts.sh
@@ -7,6 +7,7 @@ facts=(
 	"The state tree of Washington is the Western Hemlock."
 	"All mammals get goosebumps."
 	"Queen Alexandra's Birdwing is the largest butterfly in the world: females can reach wingspans of 28cm (11 inches)."
+    "There is a GitHub office in Bellevue, Washington."
 )
 
 # array to keep track of which facts have been viewed
